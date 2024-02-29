9e48b1924db5028cf0ab8fcb617ebb77c5a9bd3d
CTY111753155 <git config --global user.email youremail@example.com>
2024-02-29 14:11:25 +0800

    modify variable name

--- controllers/currency.js
+++ controllers/currency.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
-const express = require('express');
-const app = express();
+
 
 exports.getIndex = ('/',(req, res, next) => {
     res.json({"msg": "success"});
@@ -10,7 +9,7 @@ exports.getCurrency =('/exchange', (req, res, next)=>{
   const source = req.query.source;
   const target = req.query.target;
   const amountString = req.query.amount;
-  const amount = Number(amountString.replace(/\D/g, ''));
+  const amount = Number(amountString.replace(/[^\d.]/g, ''));
   fs.readFile('currency.json', 'utf8', (err, data) => {
     if (err) {
       console.error('讀取檔案時發生錯誤:', err);
@@ -22,13 +21,12 @@ exports.getCurrency =('/exchange', (req, res, next)=>{
       const rate = Number(jsonData.currencies[source][target]);
       const currency = Math.round(amount * rate * 100) / 100;
       const newcurrency = currency.toLocaleString();
-      console.log('讀取的JSON資料:', currency );
       res.json({
         "msg": "success", 
         "amount": "$"+ newcurrency
         });
     } catch (err) {
-      console.error('解析JSON時發生錯誤:', err);
+      console.error(err);
     }
   });
 });
